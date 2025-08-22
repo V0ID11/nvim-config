6c65162e8cb827d51463ff14ee74bc44d05568ae
0x-be11 <sjpardoe1@gmail.com>
2025-08-22 20:07:20 +0100

    Separated autocmds to new file and added autocmds for Haskell

---
+++ lua/sams/autocmds.lua
@@ -0,0 +1,23 @@
+local generalSettingsGroup = vim.api.nvim_create_augroup('General settings', {clear = true})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = generalSettingsGroup,
+	pattern = {'*.hs'},
+	callback = function()
+		vim.opt.tabstop = 2
+		vim.opt.shiftwidth = 2
+		vim.opt.expandtab = true
+		print('Haskell mode...')
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufLeave", {
+	group = generalSettingsGroup,
+	pattern = {'*.hs'},
+	callback = function()
+		vim.opt.tabstop = 4
+		vim.opt.shiftwidth = 4
+		vim.opt.expandtab = false
+		print('Normal mode...')
+	end,
+})
