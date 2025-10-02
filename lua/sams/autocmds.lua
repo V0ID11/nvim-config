3899da9b325b512766deccbf0dd05621c77eecda
0x-be11 <sjpardoe1@gmail.com>
2025-10-02 01:19:31 +0100

    Edited autocmds to include more filetypes

--- lua/sams/autocmds.lua
+++ lua/sams/autocmds.lua
@@ -2,22 +2,22 @@ local generalSettingsGroup = vim.api.nvim_create_augroup('General settings', {cl
 
 vim.api.nvim_create_autocmd("BufEnter", {
 	group = generalSettingsGroup,
-	pattern = {'*.hs'},
+	pattern = {'*.{hs,asm,c,h,sh}'},
 	callback = function()
 		vim.opt.tabstop = 2
 		vim.opt.shiftwidth = 2
 		vim.opt.expandtab = true
-		print('Haskell mode...')
+		--print('2 space mode...')
 	end,
 })
 
 vim.api.nvim_create_autocmd("BufLeave", {
 	group = generalSettingsGroup,
-	pattern = {'*.hs'},
+	pattern = {'*.{hs,asm,c,h,sh}'},
 	callback = function()
 		vim.opt.tabstop = 4
 		vim.opt.shiftwidth = 4
 		vim.opt.expandtab = false
-		print('Normal mode...')
+		--print('Normal mode...')
 	end,
 })
